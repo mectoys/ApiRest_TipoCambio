6cd0d11730e8e8672cbae66711b2996c56e169a7
mectoys <mectoys2013@gmail.com>
2023-03-22 22:16:26 -0500

    Agregado 2 métodos Listar último valor t.c. e insertar nuestro valor Tipo de Cambio.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # noinspection PyUnresolvedReferences
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from config import config
 from flask_mysqldb import MySQL
 from utils.Dateformat import DateFormat
@@ -46,6 +46,42 @@ def verificar_siexsiste(fecha):
         return jsonify({'ExchangeRates': exchange})
     except Exception as ex:
         return jsonify({'mensaje': 'Error'})
+
+
+# Listar último dato T.C.
+@App.route('/lastexchange')
+def Obtener_LastExchange():
+    try:
+        cursor = conexion.connection.cursor()
+        SQL = "SELECT EX.FECHA, EX.COMPRA, EX.VENTA FROM ExchangeRate EX " \
+              " WHERE EX.FECHA IN (SELECT MAX(EX.FECHA)FROM  ExchangeRate EX)"
+        print(SQL)
+        cursor.execute(SQL)
+        datos = cursor.fetchall()
+        if datos != None:
+            exchanges = []
+            for fila in datos:
+                exchange = {'FECHA': DateFormat.convert_date(fila[0]), 'COMPRA': fila[1], 'VENTA': fila[2]}
+                exchanges.append(exchange)
+            return jsonify({'Exchange': exchange})
+        else:
+            return {"ExchangeRates": "Dato no encontrado"}
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error'})
+
+
+# Método POST :Agregar T. Cambio
+@App.route('/register', methods=['POST'])
+def register_Exchange():
+    try:
+        cursor = conexion.connection.cursor()
+        SQLINNSERT = "INSERT INTO ExchangeRate(MONEDA, COMPRA, VENTA, FECHA) VALUES('{0}',{1},{2},'{3}')".format(
+            request.json['MONEDA'], request.json['COMPRA'], request.json['VENTA'], request.json['FECHA'])
+        cursor.execute(SQLINNSERT)
+        conexion.connection.commit()
+        return {'mensaje': "dato registrado"}
+    except Exception as ex:
+        return jsonify({'mensaje': str(ex)})
 
 
 if __name__ == '__main__':
