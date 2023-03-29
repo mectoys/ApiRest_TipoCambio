050a5efef8af535b83a13eadf0e8ea86f3c2d312
mectoys <mectoys2013@gmail.com>
2023-03-28 22:32:11 -0500

    Agregado 3 métodos Listar Emails, Insertar Email, Eliminar Email

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 # noinspection PyUnresolvedReferences
+#proyecto de Automatización Exchange Rate (PAER)
 from flask import Flask, jsonify, request
 from config import config
 from flask_mysqldb import MySQL
@@ -7,10 +8,8 @@ from utils.Dateformat import DateFormat
 App = Flask(__name__)
 conexion = MySQL(App)
 
-
 def pagina_no_encontrada(error):
     return "<h1>La página que intentas acceder no existe </h1>"
-
 
 @App.route('/exchanges')
 # Creación de una vista en forma de función
@@ -25,7 +24,7 @@ def list_exchanges():
             exchange = {'MONEDA': fila[0], 'COMPRA': fila[1], 'VENTA': fila[2],
                         'FECHA': DateFormat.convert_date(fila[3])}
             exchanges.append(exchange)
-        print(exchange)
+
         return jsonify({'Exchange': exchange})
     except Exception as ex:
         return jsonify({'mensaje': 'Error'})
@@ -75,14 +74,56 @@ def Obtener_LastExchange():
 def register_Exchange():
     try:
         cursor = conexion.connection.cursor()
-        SQLINNSERT = "INSERT INTO ExchangeRate(MONEDA, COMPRA, VENTA, FECHA) VALUES('{0}',{1},{2},'{3}')".format(
+        SQLINSERT = "INSERT INTO ExchangeRate(MONEDA, COMPRA, VENTA, FECHA) VALUES('{0}',{1},{2},'{3}')".format(
             request.json['MONEDA'], request.json['COMPRA'], request.json['VENTA'], request.json['FECHA'])
-        cursor.execute(SQLINNSERT)
+        cursor.execute(SQLINSERT)
         conexion.connection.commit()
         return {'mensaje': "dato registrado"}
     except Exception as ex:
         return jsonify({'mensaje': str(ex)})
 
+#Método GET Listar todos los Email
+@App.route('/emails')
+# Creación de una vista en forma de función
+def list_emails():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT EMAIL, FECHAREGISTER FROM ExchangeRateEmail"
+        cursor.execute(sql)
+        datos = cursor.fetchall()
+        emails = []
+        for fila in datos:
+            email = {'EMAIL': fila[0], 'FECHAREGISTER': DateFormat.convert_date(fila[1])}
+            emails.append(email)
+
+        return jsonify({'email': emails})
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error'})
+
+#Método POST Insertar Email
+@App.route('/registeremail', methods=['POST'])
+def register_Email():
+    try:
+        cursor = conexion.connection.cursor()
+        SQLINSERT = "INSERT INTO ExchangeRateEmail(EMAIL, FECHAREGISTER) VALUES('{0}','{1}')".format(
+            request.json['EMAIL'], request.json['FECHAREGISTER'])
+        cursor.execute(SQLINSERT)
+        conexion.connection.commit()
+        return {'mensaje': "dato registrado"}
+    except Exception as ex:
+        return jsonify({'mensaje': str(ex)})
+
+#Método DELETE Eliminar  Email
+@App.route('/delemail/<email>', methods=['DELETE'])
+def delete_Email(email):
+    try:
+        cursor = conexion.connection.cursor()
+        SQLDELETE="DELETE FROM ExchangeRateEmail WHERE EMAIL='{0}'".format(email)
+        cursor.execute(SQLDELETE)
+        conexion.connection.commit()
+        return {'mensaje': "dato Eliminado"}
+    except Exception as ex:
+        return  jsonify({'mensaje ': str(ex)})
 
 if __name__ == '__main__':
     App.config.from_object(config['development'])
